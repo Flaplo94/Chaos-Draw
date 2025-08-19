98e53940c689fe3b5534ca3f3dc5ec7e0e6f4375
Marc Larsen <57370@student.eadania.dk>
2025-08-19 14:18:25 +0200

    enemies hit flash

--- Assets/Scripts/Enemies/EnemyHealth.cs
+++ Assets/Scripts/Enemies/EnemyHealth.cs
@@ -1,25 +1,32 @@
-using UnityEngine;
 using System;
+using UnityEngine;
 
+[RequireComponent(typeof(HitFlash))]
 public class EnemyHealth : MonoBehaviour
 {
     [SerializeField] private int maxHealth = 3;
+    [SerializeField] private bool flashOnLethalHit = true;
+
     private int currentHealth;
+    private HitFlash flash;
 
     public Action OnDeath;
 
     void Awake()
     {
         currentHealth = maxHealth;
+        flash = GetComponent<HitFlash>() ?? GetComponentInChildren<HitFlash>(true);
     }
 
     public void TakeDamage(int amount)
     {
         currentHealth -= amount;
+
+        if (flashOnLethalHit || currentHealth > 0)
+            flash?.PlayFlash();
+
         if (currentHealth <= 0)
-        {
             Die();
-        }
     }
 
     public void Heal(int amount)
