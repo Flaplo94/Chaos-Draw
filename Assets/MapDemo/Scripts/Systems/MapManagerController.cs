e67170f9cff2d5ad93e4b14cab6e7248ee316f05
Marc Larsen <57370@student.eadania.dk>
2025-11-03 16:10:30 +0100

    more set up

---
+++ Assets/MapDemo/Scripts/Systems/MapManagerController.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class MapManagerController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Generate()
+    {
+        
+    }
+
+    public void Clear()
+    {
+
+    }
+
+    public void Draw()
+    {
+
+    }
+}
